ef4e38205c101362aa294430fc75696d36828bd1
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-02-12 20:56:07 +0000

    Choices added to model

--- home/models.py
+++ home/models.py
@@ -3,7 +3,20 @@ from django.db import models
 
 class SocialMedia(models.Model):
     """ Footer social media icons """
-    name = models.CharField(max_length=150, null=True, blank=False)
+    Social_Choices = [
+        ('Facebook', 'Facebook'),
+        ('Instagram', 'Instagram'),
+        ('LinkedIn', 'LinkedIn'),
+        ('Pinterest', 'Pinterest'),
+        ('Snapchat', 'Snapchat'),
+        ('TikTok', 'TikTok'),
+        ('Twitter', 'Twitter'),
+        ('WhatsApp', 'WhatsApp'),
+        ('YouTube', 'YouTube'),
+    ]
+
+    name = models.CharField(max_length=150, null=True,
+                            blank=False, choices=Social_Choices)
     social_url = models.URLField(
         max_length=1024, default='', null=True, blank=True)
 
