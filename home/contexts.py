58deb8876bebad830d8832eb859b3c294c369f5f
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-02-12 23:12:29 +0000

    If statement removed

--- home/contexts.py
+++ home/contexts.py
@@ -7,14 +7,9 @@ def FooterContext(context):
     social_media = SocialMedia.objects.all().order_by('name')
     currentYear = datetime.now().year
 
-    if social_media.count() != 0:
-        context = {
-            'social_media': social_media,
-            'currentYear': currentYear,
-        }
-    else:
-        context = {
-            'currentYear': currentYear,
-        }
+    context = {
+        'social_media': social_media,
+        'currentYear': currentYear,
+    }
 
     return context
