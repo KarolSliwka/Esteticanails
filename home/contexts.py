aaf3bd49fd8b3558180df6a40155b6e9ca3be5a3
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-02-12 22:31:02 +0000

    Fix attempt - social_media count statement chenged

--- home/contexts.py
+++ home/contexts.py
@@ -7,7 +7,7 @@ def FooterContext(context):
     social_media = SocialMedia.objects.all().order_by('name')
     currentYear = datetime.now().year
 
-    if social_media.count != 0:
+    if social_media.count() != 0:
         context = {
             'social_media': social_media,
             'currentYear': currentYear,
