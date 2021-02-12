efaf513a70e695da7847eb1604afe8572545b5b3
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-02-12 17:55:41 +0000

    Footer context file created

--- home/contexts.py
+++ home/contexts.py
@@ -2,14 +2,19 @@ from datetime import datetime
 from .models import SocialMedia
 
 
-def FooterContext():
+def FooterContext(context):
 
     social_media = SocialMedia.objects.all().order_by('name')
     currentYear = datetime.now().year
 
-    context = {
-        'social_media': social_media,
-        'currentYear': currentYear,
-    }
+    if social_media.count != 0:
+        context = {
+            'social_media': social_media,
+            'currentYear': currentYear,
+        }
+    else:
+        context = {
+            'currentYear': currentYear,
+        }
 
     return context
