59fd949580763cfff9905d73a1abfafc7634b7ff
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-02-13 12:59:25 +0000

    Contact page view created

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,46 @@
-from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib import messages
+from django.template.loader import render_to_string
+from .forms import ContactForm
 
 
 def contact(request):
+    """ A view to render Contact Page including Contact Form """
+
+    if request.method == "POST":
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            name = contact_form.cleaned_data['name']
+            from_email = contact_form.cleaned_data['from_email']
+            subject = contact_form.cleaned_data['subject']
+            message = contact_form.cleaned_data['message']
+
+            html_msg = render_to_string(
+                'includes/emails/email.html', {'name': name, 'subject': subject,
+                                               'message': message, 'from_email': from_email})
+            try:
+                send_mail(subject, message, settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER],
+                          html_message=html_msg, fail_silently=False)
+
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+
+            return redirect('/contact', messages.success(request,
+                                                         'Dear ' + name.title() + ', thanks for your message!  \
+                                        I will replay as soon as I get there'))
+        else:
+            return redirect('/contact',
+                            messages.info(request, 'Something went wrong..'))
+
+    else:
+        contact_form = ContactForm()
 
     template = 'contact/contact.html'
-    context = {}
+    context = {
+        'contact_form': contact_form,
+    }
 
     return render(request, template, context)
