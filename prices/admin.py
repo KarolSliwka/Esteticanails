ac00a6cda83dd92df44155e7d25a719a19356a89
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-02-19 00:53:35 +0000

    Service and Service Category models created and registered

--- prices/admin.py
+++ prices/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import ServiceCategory, Service
 
-# Register your models here.
+
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+    ordering = ('name',)
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'description',
+        'price',
+        'offer',
+        'offer_price',
+    )
+
+    ordering = ('name',)
+
+
+admin.site.register(ServiceCategory, ServiceCategoryAdmin)
+admin.site.register(Service, ServiceAdmin)
--- prices/models.py
+++ prices/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class ServiceCategory(models.Model):
+    """ Service categories """
+    class Meta:
+        verbose_name_plural = 'Service Categories'
+
+    name = models.CharField(max_length=254, null=False, blank=False)
+    friendly_name = models.CharField(max_length=254, null=False, blank=False)
+
+    def __str__(self):
+        return self.friendly_name
+
+
+class Service(models.Model):
+    """ """
+    offer_choices = ((True, 'Yes'), (False, 'No'))
+
+    """ """
+    name = models.CharField(max_length=254, null=False, blank=False)
+    category = models.ForeignKey(
+        'ServiceCategory', null=True, blank=False, on_delete=models.PROTECT)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=False)
+    offer = models.BooleanField(choices=offer_choices)
+    offer_price = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
