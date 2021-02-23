8075b806bc3f3bd6bf94234ff3a0e6c5415219fe
Karol Sliwka <44789604+KarolSliwka@users.noreply.github.com>
2021-02-23 20:24:13 +0000

    Time field added

--- prices/models.py
+++ prices/models.py
@@ -22,6 +22,7 @@ class Service(models.Model):
     category = models.ForeignKey(
         'ServiceCategory', null=True, blank=False, on_delete=models.PROTECT)
     description = models.TextField(null=True, blank=True)
+    time = models.TimeField(default="00:00:00")
     price = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=False)
     offer = models.BooleanField(choices=offer_choices)
